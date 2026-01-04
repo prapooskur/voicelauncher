08d73436a06d2a29921153c91612ca7e3a1d639b
Prasiddh <61389919+prapooskur@users.noreply.github.com>
2026-01-03 23:03:05 -0800

    update app icon, optimize code

--- app/src/main/java/com/pras/voicelauncher/MainActivity.kt
+++ app/src/main/java/com/pras/voicelauncher/MainActivity.kt
@@ -8,19 +8,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val intent = Intent(Intent.ACTION_VOICE_COMMAND)
+            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         startActivity(intent)
-        finish()
-//        enableEdgeToEdge()
-//        setContent {
-//            VoicelauncherTheme {
-//                val intent = Intent(Intent.ACTION_VOICE_COMMAND)
-//                startActivity(intent)
-//                // empty surface
-//                Surface(Modifier.fillMaxSize(), color = Color.DarkGray) {
-//
-//                }
-//                finish()
-//            }
-//        }
+        finishAndRemoveTask()
     }
+
 }
